e78250ab114d01b9e5dcb6ee7aac0ddd86db4442
applqpak <applqpak@gmail.com>
2016-08-21 17:00:19 +1000

    Create post_message.php

---
+++ src/post_message.php
@@ -0,0 +1,26 @@
+<?php
+
+  include "db/connect.php";
+
+  if(isset($_GET["user"]) and isset($_GET["message"]))
+  {
+
+    $user = $conn->real_escape_string($_GET["user"]);
+
+    $message = $conn->real_escape_string($_GET["message"]);
+
+    $sql = "INSERT INTO forum (user, message, date) VALUES ('" . $user . "', '" . $message . "', NOW());";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->execute();
+
+    $stmt->close();
+
+    $conn->close();
+
+  }
+
+  header("Location: index.php");
+
+?>
